790fb68ab44b2f87a68835e31c172d1c505acc7f
Sind <thesind93@gmail.com>
2015-03-12 00:35:23 +0100

    Spring 2015 theme update

--- calendar/index.php
+++ calendar/index.php
@@ -2,30 +2,31 @@
 <?php include_once("../common/headermenu.html"); ?>
 <div id='body' style='background-color: #a7e1ed;'>
 	<p><strong>Timetable not yet finalized.</strong></p>
-	<p>Calendar below is from last year, to give you a general idea.</p>
+	<p>All times are subject to change.</p>
 	<table id='timetable'>
 		
-		<th colspan='2'><strong>Friday 24th</strong></th>
+		<th colspan='2'><strong>Friday 10th</strong></th>
 		<tr><td class='timestamp'>14:00</td><td>Registrations open & matchmaking</td></tr>
-		<tr><td class='timestamp'>14:00</td><td>Talk: Introduction to Processing: 2D graphics with java</td></tr>
-		<tr><td class='timestamp'>15:00</td><td>Talk: Introduction to LÖVE: rapid 2D game development with lua</td></tr>
-		<tr><td class='timestamp'>16:00</td><td>Talk: Introduction to GIT: If it's not in source control, it doesn't exist</td></tr>
-		<tr><td class='timestamp'>16:30</td><td>Talk: Introduction to LibGDX: making crossplatform games with java</td></tr>
+		<tr><td class='timestamp'>14:30</td><td>Talk: Introduction to LibGDX: making crossplatform games with java</td></tr>
+		<tr><td class='timestamp'>15:30</td><td>Talk: Introduction to LÖVE: rapid 2D game development with lua</td></tr>
+		<tr><td class='timestamp'>16:30</td><td>Talk: Introduction to GIT: If it's not in source control, it doesn't exist</td></tr>
 		<tr><td class='timestamp'>17:00</td><td>Theme announcement & starting shot</td></tr>
-		<tr><td class='timestamp'>18:00</td><td>Talk: Introduction to making games and UIs with Qt Quick</td></tr>
-		<tr><td class='timestamp'>24:00</td><td>Time to go home</td></tr>
+		<tr><td class='timestamp'>17:30</td><td>Talk: Introduction to Processing: 2D graphics with java</td></tr>
+<!-- 		<tr><td class='timestamp'>18:00</td><td>Talk: Introduction to making games and UIs with Qt Quick</td></tr>
+		<tr><td class='timestamp'>24:00</td><td>Time to go home</td></tr> -->
 		
-		<th colspan='2'><strong>Saturday 25th</strong></th>
-		<tr><td class='timestamp'>10:00</td><td>Doors open</td></tr>
-		<tr><td class='timestamp'>13:00</td><td>Talk: A quick guide to pixelart: gotchas and techniques to rapidly improve you art</td></tr>
-		<tr><td class='timestamp'>14:00</td><td>Talk: Introduction to 2D animation: making a walkcycle (Unconfirmed)</td></tr>
-		<tr><td class='timestamp'>15:00</td><td>Talk: Introduction to game physics</td></tr>
+		<th colspan='2'><strong>Saturday 21th</strong></th>
+		<!-- <tr><td class='timestamp'>10:00</td><td>Doors open</td></tr> -->
+		<tr><td class='timestamp'>14:30</td><td>Talk: A quick guide to pixelart: gotchas and techniques to rapidly improve you art</td></tr>
+		<tr><td class = 'timestamp'>15:00</td><td>Talk: A guite to Juice: How to quickly improve the satisfaction the game gives you</td>
+		<tr><td class='timestamp'>15:30</td><td>Talk: Introduction to shaders -- fancy effects for your game</td></tr>
+		<!-- <tr><td class='timestamp'>15:00</td><td>Talk: Introduction to 2D animation: making a walkcycle (Unconfirmed)</td></tr>
+		<tr><td class='timestamp'>15:30</td><td>Talk: Introduction to game physics</td></tr>
 		<tr><td class='timestamp'>16:00</td><td>Talk: Music for your games</td></tr>
-		<tr><td class='timestamp'>17:00</td><td>Talk: Introduction to shaders -- fancy effects for your game</td></tr>
-		<tr><td class='timestamp'>24:00</td><td>Time to go home</td></tr>
+		<tr><td class='timestamp'>24:00</td><td>Time to go home</td></tr> -->
 		
 		<th colspan='2'><strong>Sunday 26th</strong></th>
-		<tr><td class='timestamp'>10:00</td><td>Doors open</td></tr>
+		<!-- <tr><td class='timestamp'>10:00</td><td>Doors open</td></tr> -->
 		<tr><td class='timestamp'>17:00</td><td>Submission deadline</td></tr>
 		<tr><td class='timestamp'>17:15</td><td>Showoff on the big screen</td></tr>
 		<tr><td class='timestamp'>Afterwards</td><td>Prizes & pizza</td></tr>
