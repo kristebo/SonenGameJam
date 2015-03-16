ab4d0a744a2af87d04e7dcf900a8ab672849b6a5
Sind <thesind93@gmail.com>
2015-03-16 16:44:18 +0100

    updated prizes

--- prizes/index.php
+++ prizes/index.php
@@ -5,23 +5,30 @@
 		<table id='prizes'>
 			
 			<th><strong>Books</strong></th>
-			<tr><td><a href='http://www.amazon.com/G%C3%B6del-Escher-Bach-Eternal-Golden/dp/0465026567/ref=sr_1_1?ie=UTF8&qid=1376162383&sr=8-1&keywords=geb'>Gödel, Escher, Bach</a></td></tr>
-			<tr><td><a href='http://www.amazon.com/Land-Lisp-Learn-Program-Game/dp/1593272812/ref=sr_1_1?ie=UTF8&qid=1376162559&sr=8-1&keywords=land+of+lisp'>Land of Lisp</a></td></tr>
-			<tr><td><a href='http://www.amazon.com/Real-Time-Rendering-Third-Edition-Akenine-Moller/dp/1568814240'>Real-Time Rendering</a></td></tr>
-			<tr><td><a href='http://www.amazon.com/C-Programming-Language-2nd-Edition/dp/0131103628/ref=sr_1_1?ie=UTF8&qid=1376065337&sr=8-1&keywords=c+programming'>The C Programming Language</a></td></tr>
-			<tr><td><a href='http://www.amazon.com/The-Pragmatic-Programmer-Journeyman-Master/dp/020161622X'>The Pragmatic Programmer</a></td></tr>
-			
+			<tr><td><a href='http://www.amazon.com/G%C3%B6del-Escher-Bach-Eternal-Golden/dp/0465026567/'>Gödel, Escher, Bach</a></td></tr>
+			<tr><td><a href='http://www.amazon.com/Land-Lisp-Learn-Program-Game/dp/1593272812/'>Land of Lisp</a></td></tr>
+			<tr><td><a href='http://www.amazon.com/Real-Time-Rendering-Third-Edition-Akenine-Moller/dp/1568814240/'>Real-Time Rendering</a></td></tr>
+			<tr><td><a href='http://www.amazon.com/C-Programming-Language-2nd-Edition/dp/0131103628/'>The C Programming Language</a></td></tr>
+			<tr><td><a href='http://www.amazon.com/The-Pragmatic-Programmer-Journeyman-Master/dp/020161622X/'>The Pragmatic Programmer</a></td></tr>
+			<tr><td><a href="http://www.amazon.com/Animators-Survival-Kit-Principles-Classical/dp/086547897X/">The Animators Survival Kit</a></td></tr>
+			<tr><td><a href="http://www.amazon.com/Drawing-Basics-Video-Game-Art/dp/0823098478/">Drawing Basics and Video Game Art</a></td></tr>
+
+
 			<th><strong>Intangible goods</strong></th>
 			<tr><td><a href='http://store.steampowered.com/'>Games on steam for 300,-</a></td></tr>
 			
 			<th><strong>Hardware</strong></th>
 			<tr><td><a href='http://arduino.cc/en/Main/ArduinoStarterKit'>Arduino Starter Kit</a></td></tr>
 			<tr><td><a href='http://www.raspberrypi.org/'>Raspberry Pi</a></td></tr>
+			<tr><td><a href="http://www.huion-tablet.com/product/product.php?sku=1013">Huion H610 Pro Drawing Pen Tablet</a></td></tr>
 			
 			<th><strong>Toys</strong></th>
 			<tr><td><a href='https://www.thinkgeek.com/product/60b6/'>Airzooka Air gun</a></td></tr>
 			<tr><td><a href='https://www.thinkgeek.com/product/dd8a/'>"Don't Panic" towel</a></td></tr>
 			<tr><td><a href='https://www.thinkgeek.com/product/eafc/'>Gyroscope</a></td></tr>
+			<tr><td><a href="http://www.thinkgeek.com/product/ef0b/">Useless Machine</a></td></tr>
+			<tr><td><a href="http://www.thinkgeek.com/product/1d75/">Flappy Bird Plush</a></td></tr>
+			<tr><td><a href="http://www.thinkgeek.com/product/ee3c/">Lego Tea Cup</a></td></tr>
 		</table>
 	</div>
 <?php include_once("../common/footer.html"); ?>
