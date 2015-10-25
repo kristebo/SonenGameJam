cff86caf2a9d313f9f8d7bd40d808752479c231e
Sind <thesind93@gmail.com>
2015-10-25 18:00:56 +0100

    link to doc added

---
+++ 2015games/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="1;url=https://docs.google.com/document/d/1GlJ7-6zasthn2yUHmwANRdBdYjw7M-ucqcdTWyJABXE/edit?usp=sharing">
+        <script type="text/javascript">
+            window.location.href = "https://docs.google.com/document/d/1GlJ7-6zasthn2yUHmwANRdBdYjw7M-ucqcdTWyJABXE/edit?usp=sharing"
+        </script>
+        <title>Page Redirection</title>
+    </head>
+    <body>
+        <!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->
+        If you are not redirected automatically, follow <a href='https://docs.google.com/document/d/1GlJ7-6zasthn2yUHmwANRdBdYjw7M-ucqcdTWyJABXE/edit?usp=sharing'>this link</a>
+    </body>
+</html>
